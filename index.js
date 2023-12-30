274f939300d5380df312f9a586a14811d0ecf438
Rv Ranjan <info.krranjan@gmail.com>
2023-12-30 18:09:17 +0530

    some changes

--- index.js
+++ index.js
@@ -1,60 +1,63 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 const app = express();
 
 // Connect to MongoDB
-mongoose.connect('mongodb://localhost/blog', { useNewUrlParser: true, useUnifiedTopology: true });
-mongoose.connection.on('error', (error) => console.error(error));
-mongoose.connection.once('open', () => console.log('Connected to MongoDB'));
+mongoose.connect("mongodb://localhost/blog", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+mongoose.connection.on("error", (error) => console.error(error));
+mongoose.connection.once("open", () => console.log("Connected to MongoDB"));
 
 // Define a Post schema
 const postSchema = new mongoose.Schema({
-    title: String,
-    content: String,
+  title: String,
+  content: String,
 });
 
-const Post = mongoose.model('Post', postSchema);
+const Post = mongoose.model("Post", postSchema);
 
 // Middleware
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static('public'));
-app.set('view engine', 'ejs');
+app.use(express.static("public"));
+app.set("view engine", "ejs");
 
 // Sample AI-related posts
 const samplePosts = [
-    // ... (same as before)
+  // ... (same as before)
 ];
 
 // Populate the database with sample posts
 Post.insertMany(samplePosts, (err) => {
-    if (err) {
-        console.error(err);
-    } else {
-        console.log('Sample posts added to the database');
-    }
+  if (err) {
+    console.error(err);
+  } else {
+    console.log("Sample posts added to the database");
+  }
 });
 
 // Routes
-app.get('/', async (req, res) => {
-    const posts = await Post.find();
-    res.render('home', { posts });
+app.get("/", async (req, res) => {
+  const posts = await Post.find();
+  res.render("home", { posts });
 });
 
-app.get('/post/:id', async (req, res) => {
-    const post = await Post.findById(req.params.id);
-    res.render('post', { post });
+app.get("/post/:id", async (req, res) => {
+  const post = await Post.findById(req.params.id);
+  res.render("post", { post });
 });
 
-app.get('/write', (req, res) => {
-    res.render('write');
+app.get("/write", (req, res) => {
+  res.render("write");
 });
 
-app.post('/write', async (req, res) => {
-    const { title, content } = req.body;
-    const newPost = new Post({ title, content });
-    await newPost.save();
-    res.redirect('/');
+app.post("/write", async (req, res) => {
+  const { title, content } = req.body;
+  const newPost = new Post({ title, content });
+  await newPost.save();
+  res.redirect("/");
 });
 
 // Start the server
